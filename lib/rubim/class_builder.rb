21e8462e49c692a31aab192349919d5bc1958735
Andrei Potetiurin <sickphilosopher@gmail.com>
2017-08-29 01:51:29 +0300

    fix class_builder & added js_class

--- lib/rubim/class_builder.rb
+++ lib/rubim/class_builder.rb
@@ -1,35 +1,42 @@
-class ClassBuilder
-	def initialize(elem_delim = '__', mod_delim = '--', value_delim = '_')
-		@mod_delim = mod_delim
-		@elem_delim = elem_delim
-		@value_delim = value_delim
-	end
-
-	def build(block, elem)
-		if (!elem)
-			return block
-		else
-			return block + @elem_delim + elem
+module Rubim
+	class ClassBuilder
+		def initialize(elem_delim = '__', mod_delim = '--', value_delim = '_', js_class='js-bem')
+			@mod_delim = mod_delim
+			@elem_delim = elem_delim
+			@value_delim = value_delim
+			@js_class = js_class
 		end
-	end
 
-	def build_mod_postfix(mod_name, mod_val)
-		res = @mod_delim + mod_name
-		res += @val_delim + mod_val if mod_val != true
-		res
-	end
+		def js_class
+			@js_class
+		end
 
-	def build_block_class(name, mod_name, modVal)
-		res = name
-		res += this.build_mod_postfix(mod_name, mod_val) if (mod_val)
-		return res
-	end
+		def build(block, elem = nil)
+			if (!elem)
+				return block
+			else
+				return block + @elem_delim + elem
+			end
+		end
 
-	def build_elem_class(block, name, mod_name, mod_val)
-		build_block_class(block) + @elem_delim + name + this.build_mod_postfix(mod_name, mod_val)
-	end
+		def build_mod_postfix(mod_name, mod_val)
+			res = %(#{@mod_delim}#{mod_name})
+			res.concat %(#{@value_delim}#{mod_val}) if mod_val != true
+			res
+		end
 
-	def split(key)
-		return key.split(@elem_delim, 2)
+		def build_block_class(block, mod_name = nil, mod_val = nil)
+			res = block
+			res += build_mod_postfix(mod_name, mod_val) if mod_val
+			return res
+		end
+
+		def build_elem_class(block, name, mod_name, mod_val)
+			build_block_class(block) + @elem_delim + name + build_mod_postfix(mod_name, mod_val)
+		end
+
+		def split(key)
+			return key.split(@elem_delim, 2)
+		end
 	end
 end
