1036d1b43f1357497277ca9b1ccb71001045994b
Andrei Potetiurin <sickphilosopher@gmail.com>
2017-08-29 01:52:03 +0300

    added attrs to entry

--- lib/rubim/entry.rb
+++ lib/rubim/entry.rb
@@ -1,5 +1,5 @@
 module Rubim
-	BEM_ENTRY_FIELDS = %i(block elem content tag mix js mods)
+	BEM_ENTRY_FIELDS = %i(block elem content tag mix js mods attrs)
 	Entry = Struct.new(*BEM_ENTRY_FIELDS, :opts) do
 		def self.from_hash(hash)
 			fields = Rubim::BEM_ENTRY_FIELDS.map{ |field| hash.delete(field)  }
