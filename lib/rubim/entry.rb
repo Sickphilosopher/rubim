4391ed3823539f2a5f17cde9dc17ca81763510b9
Andrei Potetiurin <sickphilosopher@gmail.com>
2017-11-10 19:28:27 +0300

    remove space

--- lib/rubim/entry.rb
+++ lib/rubim/entry.rb
@@ -2,7 +2,7 @@ module Rubim
 	BEM_ENTRY_FIELDS = %i(block elem content tag mix js mods attrs)
 	Entry = Struct.new(*BEM_ENTRY_FIELDS, :opts) do
 		def self.from_hash(hash)
-			fields = Rubim::BEM_ENTRY_FIELDS.map{ |field| hash.delete(field)  }
+			fields = Rubim::BEM_ENTRY_FIELDS.map{ |field| hash.delete(field) }
 			entry = Rubim::Entry.new(*fields)
 			entry.opts = hash if hash.length > 0
 			entry
