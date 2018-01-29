68b2a629bff55133e9a375e07c599a36aeaff7ec
Andrei Potetiurin <sickphilosopher@gmail.com>
2018-01-29 22:13:53 +0300

    unworking bundle spec

--- lib/rubim/bundle.rb
+++ lib/rubim/bundle.rb
@@ -2,15 +2,35 @@ module Rubim
 	class Bundle
 		def initialize(levels: {})
 			@levels = levels
-			@templates = []
 		end
 
 		def get_templates(entry)
 			@templates.select {|t| t.applicable? entry }
 		end
 
+		def templates
+			@levels.collect {|name, level| level[:templates]}
+		end
+
 		def add(template)
 			@templates << template
+		end
+
+		def load
+			@levels.each{ |l| load_level(l) }
+		end
+
+	private
+
+		def load_level(level)
+			p level
+			blocks_dirs = get_dirs(level)
+			blocks_dirs.each do |block_dir|
+			end
+		end
+
+		def get_dirs(dir)
+			p Dir["#{dir}/"]
 		end
 	end
 end
--- spec/rubim/bundle_spec.rb
+++ spec/rubim/bundle_spec.rb
@@ -1,3 +1,4 @@
+require 'fakefs/safe'
 describe Rubim::Bundle do
 	let(:bundle) {described_class.new}
 	context '#get_templates' do
@@ -13,6 +14,44 @@ describe Rubim::Bundle do
 			bundle.add template
 
 			expect(bundle.get_templates(nil)).to eq []
+		end
+
+		it 'collect all templates from level', fakefs: true do
+			FakeFS do
+				module Blocks
+				end
+				Dir.mktmpdir do |tmp_dir|
+					block = :body
+					level = :level
+					level_folder = File.join(tmp_dir, 'blocks', level.to_s)
+					block_folder = File.join(level_folder, block.to_s)
+					block_file = File.join(block_folder, block.to_s + '.bl')
+
+					FileUtils.mkdir_p(block_folder)
+					block_code = <<-BLOCK
+						Rubim::Template.match({block: '#{block}'}) do
+						end
+					BLOCK
+					File.write(block_file, block_code)
+
+					level_module = Module.new
+					Blocks.const_set(level.to_s.capitalize.to_sym, level_module)
+
+					block_template = level_module.class_eval(block_code)
+					level_module.const_set(block.to_s.capitalize.to_sym, block_template)
+
+					bundle = Rubim::Bundle.new({
+						levels: [
+							level: level_folder
+						]
+					})
+
+					bundle.load()
+
+					templates = bundle.get_templates({block: block})
+					expect(bundle.templates).to include block_template
+				end
+			end
 		end
 	end
 end
